e08628cac3d29f636e1709a209744bbdcd5f5a60
Christopher Albert <02c.albert@gmail.com>
2023-02-28 08:31:48 -0800

    test js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const slideContainer = [...document.querySelectorAll('.slide-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+console.log(slideContainer)
+slideContainer.forEach((item, i) => {
+  let containerDimensions = item.getBoundingClientRect();
+  let containerWidth = containerDimensions.width;
+
+  nxtBtn[i].addEventListener('click', () => {
+    item.scrollLeft += containerWidth
+  })
+
+  preBtn[i].addEventListener('click', () => {
+    item.scrollLeft -= containerWidth
+  })
+})
+
