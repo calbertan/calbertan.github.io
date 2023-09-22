f503082c94bc04ae8c3a8c5608bcd51a42850f2d
calbertan <02c.albert@gmail.com>
2023-09-22 16:55:48 -0700

    recolor, update carousel, add experience

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-const slideContainer = [...document.querySelectorAll('.slide-container')];
-const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
-const preBtn = [...document.querySelectorAll('.pre-btn')];
+const slideContainer = [...document.getElementsByClassName('slide-container')];
+const nxtBtn = document.getElementsByClassName('nxt-btn');
+const preBtn = document.getElementsByClassName('pre-btn');
 const slider = document.getElementById("myBar");
 var interval = 100 / 6;
 var current_width = 0;
@@ -8,8 +8,21 @@ var current_width = 0;
 slideContainer.forEach((item, i) => {
   let containerDimensions = item.getBoundingClientRect();
   let containerWidth = containerDimensions.width ;
+  let btnDisabled = false;
+
+  const enableButton = () => {
+    setTimeout(() => {
+      nxtBtn.disabled = false;
+      preBtn.disabled = false;
+      btnDisabled = false;
+    }, 400)
+  }
 
   nxtBtn[i].addEventListener('click', () => {
+    if (btnDisabled) {
+      return;
+    }
+
     item.scrollLeft += containerWidth
     var target = current_width + interval
     console.log(target);
@@ -24,11 +37,18 @@ slideContainer.forEach((item, i) => {
         slider.style.width = current_width + '%'; 
       }
     }
+
+    nxtBtn.disabled = true;
+    btnDisabled = true;
+    enableButton();
   })
 
   preBtn[i].addEventListener('click', () => {
-    item.scrollLeft -= containerWidth
+    if (btnDisabled) {
+      return;
+    }
 
+    item.scrollLeft -= containerWidth
     var target = current_width - interval
     if(target < 0) target = 0;
     var id = setInterval(frame, 10)
@@ -41,8 +61,13 @@ slideContainer.forEach((item, i) => {
         slider.style.width = current_width + '%'; 
       }
     }
+
+    preBtn.disabled = true;
+    btnDisabled = true;
+    enableButton();
   })
 })
+
 
 function frame() {
   if (current_width >= current_width + 10) {
